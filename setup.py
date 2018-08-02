7130a9d0b1924c1a1094ef7f544af37bb362d731
Henryk Plötz <henryk@ploetzli.ch>
2018-08-03 01:06:27 +0200

    Add migrations to released package.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     include_package_data=True,
     keywords='django_securebox',
     name='django_securebox',
-    packages=find_packages(include=['django_securebox']),
+    packages=find_packages(include=['django_securebox', 'django_securebox.migrations']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,

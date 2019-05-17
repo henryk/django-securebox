2d6f10b507495bfdd9890a57dccf65f8ccf6155c
Henryk Plötz <henryk@ploetzli.ch>
2019-05-17 15:13:32 +0200

    Bump version: 0.1.3 → 0.1.4

--- django_securebox/__init__.py
+++ django_securebox/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Henryk Plötz"""
 __email__ = 'henryk@ploetzli.ch'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 default_app_config = 'django_securebox.apps.DjangoSecureBoxConfig'
